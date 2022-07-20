6012cebac0f12b929fe1b98a3af3535171effe4e
Darya <dasha8562@gmail.com>
2022-07-20 20:52:59 +0300

    Перламутровые пуговицы

--- js/main.js
+++ js/main.js
@@ -3,11 +3,13 @@ import { setUserFormSubmit } from './form-validation.js';
 import { getData } from './api.js';
 import { showErrorGetData } from './show-alert.js';
 import { clearForm } from './open-close-modal.js';
+import { initializeFilters } from './filter.js';
 
 const PHOTOS_COUNT = 25;
 
 getData((photos) => {
   renderPictures(photos.slice(0, PHOTOS_COUNT));
+  initializeFilters(photos);
 }, showErrorGetData);
 
 setUserFormSubmit(clearForm);
